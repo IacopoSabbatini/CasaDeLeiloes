67cba3bac3a5c110328ca947dd489528aa161096
Iacopo <iacoposabbatini@gmail.com>
2025-03-03 09:43:05 -0300

    botão de salvar cadastrando produto.

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,9 +1,10 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
 
 
 /*
@@ -16,18 +17,46 @@ import javax.swing.JOptionPane;
  * @author Adm
  */
 public class conectaDAO {
+     Connection conn;
+    PreparedStatement stm;
+    ResultSet rs;
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
+    // Login
+    String db = "jdbc:mysql://localhost:3306/uc11";
+    String user = "root";
+    String senha = "Pudim23052003!";
+
+    public void connectDB() {
+        //Connection conn = null;
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(db, user, senha);
+            System.out.println("Conexao realizada com sucesso.");
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Erro ao tentar conectar: " + ex.getMessage());
+
         }
+    }
+
+    public void desconectar() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Conexao encerrada com sucesso.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao tentar desconectar: " + ex.getMessage());
+        }
+    }
+
+    public boolean isConected() {
+        return (conn != null);
+    }
+
+    public Connection getConexao() {
         return conn;
     }
-    
+
 }
+
